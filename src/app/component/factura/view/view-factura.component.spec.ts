04833dd5f2d9d00ea0cd03d1b2a3ea10a3c7022b
elen <elen10mik@gmail.com>
2021-11-21 22:44:48 +0100

    factura service

--- src/app/component/factura/view/view-factura.component.spec.ts
+++ src/app/component/factura/view/view-factura.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ViewFacturaComponent } from './view-factura.component';
 
 describe('ViewFacturaComponent', () => {
--- src/app/service/factura.service.ts
+++ src/app/service/factura.service.ts
@@ -1,9 +1,50 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_URL, httpOptions } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacturaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  sURL = API_URL + '/factura';
+
+
+  getOne(id: number): Observable<String> {
+    return this.http.get<String>(this.sURL + "/" + id, httpOptions);
+  }
+
+  getAll(): Observable<String> {
+    return this.http.get<String>(this.sURL + "/all" , httpOptions);
+  }
+
+  Create(body:string): Observable<String> {
+    return this.http.post<String>(this.sURL ,body , httpOptions);
+  }
+
+  Delete(id: number): Observable<String> {
+    return this.http.delete<String>(this.sURL + "/" + id, httpOptions);
+  }
+
+  Count(): Observable<String> {
+    return this.http.get<String>(this.sURL + "/count", httpOptions);
+  }
+
+  Update(id: number,body:string): Observable<String> {
+    return this.http.put<String>(this.sURL + "/" + id,body, httpOptions);
+  }
+
+  getPage(): Observable<String> {
+    return this.http.get<String>(this.sURL + "/page", httpOptions);
+  }
+
+  Random(): Observable<String> {
+    return this.http.post<String>(this.sURL + "/random" , httpOptions);
+  }
+
 }
+
