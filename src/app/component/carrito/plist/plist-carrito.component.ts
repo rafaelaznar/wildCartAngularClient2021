bb31fd6edf6ee447555fd0481ef92997e5d3449f
Josevi97 <josevi00@hotmail.com>
2021-12-02 13:12:06 +0100

    carrito basic added

--- src/app/component/carrito/plist/plist-carrito.component.ts
+++ src/app/component/carrito/plist/plist-carrito.component.ts
@@ -1,15 +1,124 @@
+import { ProductoService } from './../../../service/producto.service';
+import { IPageProduct, IProducto } from 'src/app/model/producto-interfaces';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { PaginationService } from 'src/app/service/pagination.service';
+import { IconService } from 'src/app/service/icon.service';
+import { IUsuario } from 'src/app/model/usuario-interfaces';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-plist-carrito',
   templateUrl: './plist-carrito.component.html',
-  styleUrls: ['./plist-carrito.component.css']
+  styleUrls: ['./plist-carrito.component.css'],
 })
 export class PlistCarritoComponent implements OnInit {
+  strEntity: string = 'carrito';
+  strOperation: string = 'plist';
+  strTitleSingular: string = 'Carrito';
+  strTitlePlural: string = 'Carritos';
+  aProducts: IProducto[];
+  aPaginationBar: string[];
+  nTotalElements: number;
+  nTotalPages: number;
+  nPage: number;
+  nPageSize: number = 10;
+  strResult: string = null;
+  strFilter: string = '';
+  strSortField: string = '';
+  strSortDirection: string = '';
+  strFilteredMessage: string = '';
+  oUserSession: IUsuario;
+  subjectFiltro$ = new Subject();
+  id_tipoproducto: number = null;
 
-  constructor() { }
+  constructor(
+    private oRoute: ActivatedRoute,
+    private oRouter: Router,
+    private oPaginationService: PaginationService,
+    private oProductService: ProductoService,
 
-  ngOnInit(): void {
+    public oIconService: IconService
+  ) {
+    if (this.oRoute.snapshot.data.message) {
+      this.oUserSession = this.oRoute.snapshot.data.message;
+      localStorage.setItem(
+        'user',
+        JSON.stringify(this.oRoute.snapshot.data.message)
+      );
+    } else {
+      localStorage.clear();
+      oRouter.navigate(['/home']);
+    }
+    this.id_tipoproducto = this.oRoute.snapshot.params.id_tipoproducto;
+    if (this.id_tipoproducto) {
+      this.strFilteredMessage =
+        'Listado filtrado por el tipo de producto ' + this.id_tipoproducto;
+    } else {
+      this.strFilteredMessage = '';
+    }
+
+    this.nPage = 0;
+    this.getPage();
   }
 
+  ngOnInit(): void {
+    this.subjectFiltro$
+      .pipe(debounceTime(1000))
+      .subscribe(() => this.getPage());
+  }
+
+  getPage = () => {
+    console.log('buscando...', this.strFilter);
+    this.oProductService
+      .getPage(
+        this.nPageSize,
+        this.nPage,
+        this.strFilter,
+        this.strSortField,
+        this.strSortDirection,
+        this.id_tipoproducto
+      )
+      .subscribe((oPage: IPageProduct) => {
+        if (this.strFilter) {
+          this.strFilteredMessage = 'Listado filtrado: ' + this.strFilter;
+        } else {
+          this.strFilteredMessage = '';
+        }
+        this.aProducts = oPage.content;
+        this.nTotalElements = oPage.totalElements;
+        this.nTotalPages = oPage.totalPages;
+        this.aPaginationBar = this.oPaginationService.pagination(
+          this.nTotalPages,
+          this.nPage
+        );
+      });
+  };
+
+  jumpToPage = () => {
+    this.getPage();
+    return false;
+  };
+  onKeyUpFilter(event: KeyboardEvent): void {
+    this.subjectFiltro$.next();
+  }
+
+  doResetOrder() {
+    this.strSortField = '';
+    this.strSortDirection = '';
+    this.getPage();
+  }
+
+  doSetOrder(order: string) {
+    this.strSortField = order;
+    if (this.strSortDirection == 'asc') {
+      this.strSortDirection = 'desc';
+    } else if (this.strSortDirection == 'desc') {
+      this.strSortDirection = '';
+    } else {
+      this.strSortDirection = 'asc';
+    }
+    this.getPage();
+  }
 }
