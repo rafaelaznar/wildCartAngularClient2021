b76144c37c2d580601565d2d0eb29f653d12f51c
Josevi97 <josevi00@hotmail.com>
2021-12-02 10:35:07 +0100

    tipousuario plist updated

--- src/app/service/tipousuario.service.ts
+++ src/app/service/tipousuario.service.ts
@@ -5,69 +5,74 @@ import { Router } from '@angular/router';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import {
-	API_URL,
-	environment,
-	httpOptions,
+  API_URL,
+  environment,
+  httpOptions,
 } from 'src/environments/environment';
 import { ITipoUsuarioPage, IUserType } from '../model/tipousuario-interfaces';
 
 @Injectable({
-	providedIn: 'root',
+  providedIn: 'root',
 })
 export class TipousuarioService {
-	constructor(private http: HttpClient, private oRoute: Router) {}
+  constructor(private http: HttpClient, private oRoute: Router) {}
 
-	sURL = API_URL + '/tusuario';
+  sURL = API_URL + '/tusuario';
 
-	handleError(error: HttpErrorResponse) {
-		let errorMessage = 'Unknown error!';
-		if (error.error instanceof ErrorEvent) {
-			// Client-side errors
-			errorMessage = `Error: ${error.error.message}`;
-			if (environment) console.log('SessionService: error: ' + errorMessage);
-		} else {
-			// Server-side errors
-			errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-			if (environment) console.log('SessionService: error: ' + errorMessage);
-		}
-		return throwError(errorMessage);
-	}
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+      if (environment) console.log('SessionService: error: ' + errorMessage);
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+      if (environment) console.log('SessionService: error: ' + errorMessage);
+    }
+    return throwError(errorMessage);
+  }
 
-	view(id: number): Observable<IUserType> {
-		return this.http
-			.get<IUserType>(`${this.sURL}/${id}`, httpOptions)
-			.pipe(catchError(this.handleError));
-	}
+  view(id: number): Observable<IUserType> {
+    return this.http
+      .get<IUserType>(`${this.sURL}/${id}`, httpOptions)
+      .pipe(catchError(this.handleError));
+  }
 
-	plist(
-		page: number,
-		size: number,
-		orderBy: string,
-		orderAs: boolean
-	): Observable<ITipoUsuarioPage> {
-		const sort: string = `${orderBy},${orderAs ? 'asc' : 'desc'}`;
-		return this.http
-			.get<ITipoUsuarioPage>(
-				`${this.sURL}/page?page=${page}&size=${size}&sort=${sort}`,
-				httpOptions
-			)
-			.pipe(catchError(this.handleError));
-	}
+  getPage(
+    rpp: number,
+    page: number,
+    filter: string,
+    order: string,
+    direction: string
+  ): Observable<ITipoUsuarioPage> {
+    let strUrl: string = '';
+    if (filter) {
+      strUrl += '&filter=' + filter;
+    }
+    if (order) {
+      strUrl += '&sort=' + order + ',' + direction;
+    }
+    return this.http.get<ITipoUsuarioPage>(
+      this.sURL + '/?page=' + (page - 1) + '&size=' + rpp + strUrl,
+      httpOptions
+    );
+  }
 
-	edit(body: String): Observable<IUserType> {
-		return this.http
-			.put<IUserType>(this.sURL, body, httpOptions)
-			.pipe(catchError(this.handleError));
-	}
-	postJsonFormater(formPost: FormGroup): string {
-		let data: any = {};
-		Object.keys(formPost.controls).forEach((key) => {
-			data[key] = formPost.controls[key].value;
-		});
-		return JSON.stringify(data);
-	}
+  edit(body: String): Observable<IUserType> {
+    return this.http
+      .put<IUserType>(this.sURL, body, httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+  postJsonFormater(formPost: FormGroup): string {
+    let data: any = {};
+    Object.keys(formPost.controls).forEach((key) => {
+      data[key] = formPost.controls[key].value;
+    });
+    return JSON.stringify(data);
+  }
 
-	redirectPlist(): void {
-		this.oRoute.navigate(['/tipousuario/plist']);
-	}
+  redirectPlist(): void {
+    this.oRoute.navigate(['/tipousuario/plist']);
+  }
 }
