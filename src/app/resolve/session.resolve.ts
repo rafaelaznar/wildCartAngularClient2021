d7a1dc889646cf01b81752e9de0e896b837cbace
rafaelaznar <rafaaznar@gmail.com>
2022-12-30 08:52:05 +0100

    arreglos

--- src/app/resolve/session.resolve.ts
+++ src/app/resolve/session.resolve.ts
@@ -3,7 +3,6 @@ import { Resolve } from '@angular/router';
 import { throwError } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { of } from 'rxjs/internal/observable/of';
-
 import { catchError } from 'rxjs/internal/operators/catchError';
 import { tap } from 'rxjs/internal/operators/tap';
 import { IUsuario } from '../model/usuario-interfaces';
@@ -16,9 +15,10 @@ export class SessionResolver implements Resolve<Observable<IUsuario>> {
     constructor(private oSessionService: SessionService) { }
 
     resolve(): Observable<IUsuario> {
-        return this.oSessionService.check().pipe(
-            // tap((u: IUsuario) => console.log("session.service check HTTP request executed: ", u.nombre)),
-            catchError(err => of(null))
-        );
+        return this.oSessionService.check()
+            .pipe(
+                // tap((u: IUsuario) => console.log("session.service check HTTP request executed: ", u.nombre)),
+                catchError(err => of(null))
+            );
     }
 }
