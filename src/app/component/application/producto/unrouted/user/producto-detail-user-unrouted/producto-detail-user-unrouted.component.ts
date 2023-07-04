2e54b4d28ccfa68176664462ba09fef705e63864
rafaelaznar <rafaaznar@gmail.com>
2023-07-04 07:43:34 +0200

    informe de producto

--- src/app/component/application/producto/unrouted/user/producto-detail-user-unrouted/producto-detail-user-unrouted.component.ts
+++ src/app/component/application/producto/unrouted/user/producto-detail-user-unrouted/producto-detail-user-unrouted.component.ts
@@ -5,7 +5,9 @@ import { IProducto } from 'src/app/model/producto-interfaces';
 import { CarritoService } from 'src/app/service/carrito.service';
 import { MetadataService } from 'src/app/service/metadata.service';
 import { ProductoService } from 'src/app/service/producto.service';
+import { API_URL } from 'src/environments/environment';
 
+declare let jsPDF: any;
 @Component({
   selector: 'app-producto-detail-user-unrouted',
   templateUrl: './producto-detail-user-unrouted.component.html',
@@ -18,6 +20,7 @@ export class ProductoDetailUserUnroutedComponent implements OnInit {
   @Input() id_tipousuario_session: number = null;
   @Output() addCarritoEE = new EventEmitter<number>();
 
+  API_URL: string = API_URL;
   oProducto: IProducto;
   oNewCommentResult: IResult = null;
 
@@ -61,7 +64,7 @@ export class ProductoDetailUserUnroutedComponent implements OnInit {
       if (this.oNewCommentResult.id > 0) {
         // recargar el listado de comentarios
         this.reloadCommentsSubject.next(true);
-        
+
       } else {
         this.openPopup('Error en la creación de ' + this.oMetadataService.getName('the' + this.oNewCommentResult.strEntity).toLowerCase());
       }
@@ -69,6 +72,51 @@ export class ProductoDetailUserUnroutedComponent implements OnInit {
       this.openPopup('ERROR: ' + this.oNewCommentResult.error.status + ': ' + this.oNewCommentResult.error.message);
     }
   };
+
+  print(id: number) {
+
+    var doc = new jsPDF()
+
+    var imgData = API_URL + '/file/' + this.oProducto?.imagen;
+    console.log(id, this.oProducto?.imagen, imgData);
+
+    //doc.addImage(imgData, 'JPG', 150, 20, 50, 50) //pendiente
+
+    doc.setFontSize(50)
+    doc.text('WildCart', 10, 25)
+
+    doc.setFontSize(20)
+    doc.text('Ficha Técnica de producto', 10, 35)
+
+    let font1: number = 14;
+    let font2: number = 12;
+    let alignx1: number = 10;
+    let alignx2: number = 60;
+
+    doc.setFontSize(font1)
+    doc.text('Descripción:', alignx1, 80)
+    doc.setFontSize(font2)
+    doc.text(this.oProducto.nombre, alignx2, 80)
+
+    doc.setFontSize(font1)
+    doc.text('Código:', alignx1, 90)
+    doc.setFontSize(font2)
+    doc.text(this.oProducto.codigo, alignx2, 90)
+
+    doc.setFontSize(font1)
+    doc.text('Precio:', alignx1, 100)
+    doc.setFontSize(font2)
+    doc.text(this.oProducto.precio.toString(), alignx2, 100)
+
+    doc.setFontSize(font1)
+    doc.text('Tipo de producto:', alignx1, 110)
+    doc.setFontSize(font2)
+    doc.text(this.oProducto.tipoproducto.nombre, alignx2, 110)
+
+
+    doc.save(this.oProducto.codigo + "- Ficha Tecnica.pdf");
+
+  }
 
   //popup
 
@@ -79,7 +127,7 @@ export class ProductoDetailUserUnroutedComponent implements OnInit {
   }
 
   onClosePopup(): void {
-    
+
   }
 
 }
